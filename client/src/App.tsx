7f1b70832fa8f40dd715e69916187f1e55f8b0a2
Prashant Kumar <71703153+Prashantkry@users.noreply.github.com>
2024-06-13 01:45:50 +0530

    updated data

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,7 +3,6 @@ import { Route, Routes } from 'react-router-dom'
 import './App.css'
 import HomePage from './pages/HomePage'
 import Layout from './Layout'
-import { Plans } from './pages/Plans'
 import SignIn from './pages/SignIn'
 import SignUp from './pages/SignUp'
 import Game from './pages/Game'
@@ -16,7 +15,6 @@ function App() {
         <Route path='' element={<Layout />}>
           <Route path='/' element={<HomePage />}></Route>
           <Route path='/game' element={<Game />}></Route>
-          <Route path='/Plans' element={<Plans />}></Route>
           <Route path='/signIn' element={<SignIn />}></Route>
           <Route path='/signUp' element={<SignUp />}></Route>
         </Route>
